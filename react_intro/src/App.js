6850b1ce3687789ac7ab6fa1d9c0f0422ecbfe90
Abhishek Silawat <91587618+abhishek-0607@users.noreply.github.com>
2022-01-02 00:31:05 +0530

    Update App.js

--- react_intro/src/App.js
+++ react_intro/src/App.js
@@ -1,35 +1,27 @@
-
-import './App.css';
-
+import "./App.css";
 
 function App() {
-  const arr = ["Android","Blackberry","iPhone","Windows Phone"];
-  const arr2 = ["Samsung", "Apple","HTC","Micromax"]
-
-
+  const arr = ["Android", "Blackberry", "iPhone", "Windows Phone"];
+  const arr2 = ["Samsung", "Apple", "HTC", "Micromax"];
 
   return (
     <div className="App">
-
-
       <h1>Mobile Operating System</h1>
-      <List val= {arr}/>
+      <List val={arr} />
 
       <h1>Mobile Manufacturers</h1>
-      <List val= {arr2}/>
-      
-
-
+      <List val={arr2} />
     </div>
   );
 }
-function List({val}){
-  return <ul>
-    {val.map((e)=>(<li>{e}</li>))}
-  </ul>
+function List({ val }) {
+  return (
+    <ul>
+      {val.map((e) => (
+        <li>{e}</li>
+      ))}
+    </ul>
+  );
 }
 
-
 export default App;
-
-
