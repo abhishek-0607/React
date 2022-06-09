e523a3a0350d6f984e7d04df67eb1f574cfe3f63
Abhishek Silawat <absilawat@gmail.com>
2022-06-09 10:54:36 +0530

    commit

--- practice_projects/list_counter/src/components/NewTodo/Todo.jsx
+++ practice_projects/list_counter/src/components/NewTodo/Todo.jsx
@@ -6,6 +6,7 @@ import { v4 as uuid } from "uuid";
 
 function Todo() {
   const [list, setList] = useState([]);
+  const [showAll, setShowAll] = useState(true);
   const handleAdd = (data) => {
     let payload = {
       title: data,
@@ -14,12 +15,33 @@ function Todo() {
     };
     setList([...list, payload]);
   };
+
+  const handleDelete = (id) => {
+    const updatedTodo = list.filter((item) => item.id !== id);
+    setList(updatedTodo);
+  };
+  const handleToggle = (id) => {
+    const updatedTodo = list.map((item) => {
+      return item.id === id ? { ...item, status: !item.status } : item;
+    });
+    setList(updatedTodo);
+  };
   return (
     <div>
       <Todoinput handleClick={handleAdd} />
-      {list.map((item) => (
-        <Todolist {...item} />
-      ))}
+      {list
+        .filter((item) => (showAll ? true : !item.status))
+        .map((item) => (
+          <Todolist
+            handleDelete={handleDelete}
+            handleToggle={handleToggle}
+            key={item.id}
+            {...item}
+          />
+        ))}
+      <button onClick={() => setShowAll(!showAll)}>
+        {showAll ? "Show All Unfinished Task" : "Show All Tasks"}
+      </button>
     </div>
   );
 }
--- practice_projects/list_counter/src/components/NewTodo/Todolist.jsx
+++ practice_projects/list_counter/src/components/NewTodo/Todolist.jsx
@@ -1,7 +1,25 @@
 import { useState } from "react";
 import React from "react";
 
-function Todolist({ title }) {
-  return <div>{title}</div>;
+function Todolist({ title, status, id, handleToggle, handleDelete }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <button
+        onClick={() => {
+          handleToggle(id);
+        }}
+      >
+        {status ? "TRUE" : "FALSE"}
+      </button>
+      <button
+        onClick={() => {
+          handleDelete(id);
+        }}
+      >
+        REMOVE
+      </button>
+    </div>
+  );
 }
 export { Todolist };
