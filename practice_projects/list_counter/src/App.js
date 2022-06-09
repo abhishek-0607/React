16f0e2888edf3362ad62421c70037fb189f4cd1f
Abhishek Silawat <absilawat@gmail.com>
2022-06-10 03:32:03 +0530

    commit

--- practice_projects/list_counter/src/App.js
+++ practice_projects/list_counter/src/App.js
@@ -1,15 +1,38 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-import { Todo } from "./components/NewTodo/Todo";
+// import { Todo } from "./components/NewTodo/Todo";
+
+const initState = {
+  name: "",
+  age: "",
+  email: "",
+};
 
 function App() {
+  const [data, setData] = useState(initState);
+  const { name, age, email } = data;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  const handleClick = () => {
+    console.log(data);
+  };
   return (
     <div className="App">
+      <input name="name" value={name} onChange={handleChange} />
+      <input name="email" value={email} onChange={handleChange} />
+      <input name="age" value={age} onChange={handleChange} />
+
+      <button onClick={handleClick}>BUTTON</button>
+
       {/* <Todo />
       <Parenttochild />
       <Childtoparent />
       <Siblings /> */}
-      <Todo />
+
+      {/* <Todo /> */}
     </div>
   );
 }
