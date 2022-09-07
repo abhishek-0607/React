dcaea27cee0317e6c04b4bde8a3d495655cd666a
Abhishek Silawat <absilawat@gmail.com>
2022-09-07 15:21:50 +0530

    news item is corrected

--- practice_projects/bootstrep/src/components/news/Newsitem.js
+++ practice_projects/bootstrep/src/components/news/Newsitem.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 
 export default class Newsitem extends Component {
   render() {
-    let { title, description, imgUrl } = this.props;
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
           <img src={imgUrl} className="card-img-top" alt="" />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="norerror"
+              className="btn btn-sm btn-primary"
+            >
               Read more...
             </a>
           </div>
