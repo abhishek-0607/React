7538e218d2b938987c35c483960e937f455b1053
Abhishek Silawat <absilawat@gmail.com>
2022-09-10 20:07:51 +0530

    Update Navbar.js

--- practice_projects/bootstrep/src/components/news/Navbar.js
+++ practice_projects/bootstrep/src/components/news/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
@@ -6,9 +7,9 @@ export default class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              NewMonkey
-            </a>
+            <Link className="navbar-brand" to="/">
+              NewsMonkey
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -26,44 +27,44 @@ export default class Navbar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
+                  <Link className="nav-link active" aria-current="page" to="/">
                     Home
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    About
-                  </a>
+                  <Link className="nav-link" to="/general">
+                    General
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/business">
                     business
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/entertainment">
                     entertainment
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/health">
                     health
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/science">
                     science
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/sports">
                     sports
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <Link className="nav-link" to="/technology">
                     technology
-                  </a>
+                  </Link>
                 </li>
               </ul>
               {/* <form className="d-flex" role="search">
