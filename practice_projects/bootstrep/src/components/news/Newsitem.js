3f57a96f0b0e0ce5805fd2a3c564963843df9ee7
Abhishek Silawat <absilawat@gmail.com>
2022-09-15 18:50:15 +0530

    Update Newsitem.js

--- practice_projects/bootstrep/src/components/news/Newsitem.js
+++ practice_projects/bootstrep/src/components/news/Newsitem.js
@@ -2,14 +2,27 @@ import React, { Component } from "react";
 
 export default class Newsitem extends Component {
   render() {
-    let { title, description, imgUrl, newsUrl } = this.props;
+    let { title, description, imgUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div>
         <div className="card">
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left: "90%", zIndex: 1 }}
+          >
+            {source}
+            <span className="visually-hidden">unread messages</span>
+          </span>
           <img src={imgUrl} className="card-img-top" alt="" height="200px" />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                {author ? "by " + author : ""} on {new Date(date).toGMTString()}
+              </small>
+            </p>
             <a href={newsUrl} target="norerror" className="btn btn-sm btn-dark">
               Read more...
             </a>
