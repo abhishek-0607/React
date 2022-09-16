54ca3754584c5ed4b3a180d64c428ced5a071c92
Abhishek Silawat <absilawat@gmail.com>
2022-09-17 01:30:05 +0530

    Update Classcomponent.js

--- practice_projects/bootstrep/src/components/news/Classcomponent.js
+++ practice_projects/bootstrep/src/components/news/Classcomponent.js
@@ -2,19 +2,30 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Newsinfinite from "./Newsinfinite";
 import { Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export default class Classcomponent extends Component {
   pageSize = 6;
+  apikey = process.env.REACT_APP_API_KEY;
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
         <Navbar />
+        <LoadingBar color="#0d6efd" progress={this.state.progress} />
         <Routes>
           <Route
             exact
             path="/"
             element={
               <Newsinfinite
+                setProgress={this.setProgress}
+                apikey={this.apikey}
                 key="general"
                 pageSize={this.pageSize}
                 country="in"
@@ -27,6 +38,8 @@ export default class Classcomponent extends Component {
             path="/business"
             element={
               <Newsinfinite
+                setProgress={this.setProgress}
+                apikey={this.apikey}
                 key="business"
                 pageSize={this.pageSize}
                 country="in"
@@ -39,6 +52,8 @@ export default class Classcomponent extends Component {
             path="/entertainment"
             element={
               <Newsinfinite
+                setProgress={this.setProgress}
+                apikey={this.apikey}
                 key="entertainment"
                 pageSize={this.pageSize}
                 country="in"
@@ -51,6 +66,8 @@ export default class Classcomponent extends Component {
             path="/sports"
             element={
               <Newsinfinite
+                setProgress={this.setProgress}
+                apikey={this.apikey}
                 key="sports"
                 pageSize={this.pageSize}
                 country="in"
@@ -63,6 +80,8 @@ export default class Classcomponent extends Component {
             path="/science"
             element={
               <Newsinfinite
+                setProgress={this.setProgress}
+                apikey={this.apikey}
                 key="science"
                 pageSize={this.pageSize}
                 country="in"
@@ -75,6 +94,8 @@ export default class Classcomponent extends Component {
             path="/technology"
             element={
               <Newsinfinite
+                setProgress={this.setProgress}
+                apikey={this.apikey}
                 key="technology"
                 pageSize={this.pageSize}
                 country="in"
@@ -87,6 +108,8 @@ export default class Classcomponent extends Component {
             path="/health"
             element={
               <Newsinfinite
+                setProgress={this.setProgress}
+                apikey={this.apikey}
                 key="health"
                 pageSize={this.pageSize}
                 country="in"
@@ -96,7 +119,7 @@ export default class Classcomponent extends Component {
           ></Route>
         </Routes>
 
-        {/* <Newsinfinite key="" pageSize={this.pageSize} country="in" category="sports" /> */}
+        {/* <Newsinfinite setProgress={this.setprogress} apikey={this.apikey} key="" pageSize={this.pageSize} country="in" category="sports" /> */}
       </div>
     );
   }
