1b896da26ccc2e7c0614fa11106b85ec3ffbceec
Abhishek Silawat <absilawat@gmail.com>
2022-09-17 01:30:13 +0530

    Update Newsitem.js

--- practice_projects/bootstrep/src/components/news/Newsitem.js
+++ practice_projects/bootstrep/src/components/news/Newsitem.js
@@ -7,13 +7,21 @@ export default class Newsitem extends Component {
     return (
       <div>
         <div className="card">
-          <span
-            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
-            style={{ left: "90%", zIndex: 1 }}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: 0,
+            }}
           >
-            {source}
-            <span className="visually-hidden">unread messages</span>
-          </span>
+            <span
+              className="badge rounded-pill bg-danger"
+              style={{ left: "90%", zIndex: 1 }}
+            >
+              {source}
+            </span>
+          </div>
           <img src={imgUrl} className="card-img-top" alt="" height="200px" />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
