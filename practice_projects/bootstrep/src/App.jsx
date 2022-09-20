b035cc459271feba2640999fcf04fc36c285caab
Abhishek Silawat <absilawat@gmail.com>
2022-09-21 03:23:32 +0530

    Update App.jsx

--- practice_projects/bootstrep/src/App.jsx
+++ practice_projects/bootstrep/src/App.jsx
@@ -1,24 +1,18 @@
 import "./App.css";
-// import Navbar from "./components/Navbar";
-// import Textform from "./components/Textform";
-// import Counter from "./components/Counter";
-// import { Todo } from "./components/Todo/Todo";
+
 import { BrowserRouter } from "react-router-dom";
 
-import Classcomponent from "./components/classbasedNews/Classcomponent";
+// import Classcomponent from "./components/classbasedNews/Classcomponent";
+import { FunctionComponent } from "./components/functionbasedNews/FunctionComponent";
 
 function App() {
   return (
     <>
-      {/* <Navbar title="utilsText" />
-      <div className="container my-3">
-        <Textform heading="Enter the text to analyze below" />
-      </div>
-      <Counter />
-
-      <Todo /> */}
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Classcomponent />
+      </BrowserRouter> */}
+      <BrowserRouter>
+        <FunctionComponent />
       </BrowserRouter>
     </>
   );
