4048c69d3ed98dfc22fffa8c3b64c40ad50cea89
Abhishek Silawat <absilawat@gmail.com>
2022-12-15 11:36:31 +0530

    Update AppContext.jsx

--- contextapi/src/contexts/AppContext.jsx
+++ contextapi/src/contexts/AppContext.jsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState } from "react";
+
 export const AppContext = createContext();
+
 export const AppContextProvider = ({ children }) => {
   const [isAuth, setIsAuth] = useState(false);
   const [token, setToken] = useState("abcde");
