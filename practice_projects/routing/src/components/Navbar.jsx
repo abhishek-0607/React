353e9ea67c4d3ae95dd7049b8a3900b2f9761592
Abhishek Silawat <absilawat@gmail.com>
2023-04-26 00:35:27 +0530

    Create: Responsive Navbar using ChakraUI

--- practice_projects/routing/src/components/Navbar.jsx
+++ practice_projects/routing/src/components/Navbar.jsx
@@ -1,5 +1,14 @@
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Link,
+  useBoolean,
+} from "@chakra-ui/react";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 
 const links = [
   {
@@ -17,14 +26,64 @@ const links = [
 ];
 
 const Navbar = () => {
+  const [flag, setFlag] = useBoolean(false);
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      {links.map(({ to, title }) => (
-        <div style={{ margin: "10px" }}>
-          <Link to={to}>{title}</Link>
-        </div>
-      ))}
-    </div>
+    <Box>
+      <Box pos="fixed" top="1rem" right="1rem" align="center">
+        <Flex display={{ base: "none", md: "flex" }}>
+          {links.map(({ to, title }) => (
+            <Box>
+              <Link as={RouterLink} to={to}>
+                <Button variant="ghost">{title}</Button>
+              </Link>
+            </Box>
+          ))}
+        </Flex>
+        <IconButton
+          display={{ base: "block", md: "none" }}
+          icon={<HamburgerIcon />}
+          onClick={setFlag.on}
+        ></IconButton>
+      </Box>
+      {flag && (
+        <Box
+          pos="fixed"
+          top="0"
+          left="0"
+          w="100vw"
+          h="100vh"
+          zIndex={20}
+          bgColor="white"
+          overflow="auto"
+          display={{ base: "block", md: "none" }}
+        >
+          <Flex>
+            <Box pos="fixed" top="1rem" right="1rem">
+              <IconButton
+                onClick={setFlag.off}
+                display={{ base: "block", md: "none" }}
+                icon={<CloseIcon />}
+              ></IconButton>
+            </Box>
+          </Flex>
+
+          <Flex
+            direction="column"
+            align="center"
+            justify="center"
+            marginTop="3rem"
+          >
+            {links.map(({ to, title }) => (
+              <Box>
+                <Link as={RouterLink} to={to}>
+                  <Button variant="ghost">{title}</Button>
+                </Link>
+              </Box>
+            ))}
+          </Flex>
+        </Box>
+      )}
+    </Box>
   );
 };
 export { Navbar };
